1afbb07932a57eff2e2637368085f17676e372ae
Joe Brailsford <1782978+jbrailsford@users.noreply.github.com>
2021-10-21 23:20:17 +0100

    test(util): Add util tests
    Added inital tests for utilities.

---
+++ util/util_test.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var extensions = FileExtensions{
+	".hcl": nil,
+	".tf":  nil,
+}
+
+func TestFileExtensionsContains(t *testing.T) {
+	assert.True(t, extensions.Contains("hcl"), "validate FileExtensions contains value")
+}
+
+func TestFileExtensionsToCsvConversion(t *testing.T) {
+	assert.Equal(t, ".hcl, .tf", extensions.AsCommaSeparatedString(), "validate FileExtensions can be converted to a csv string")
+}
+
+func TestErrorAndExit(t *testing.T) {
+	if os.Getenv("TEST_EXIT") == "1" {
+		ErrorAndExit("testing")
+		return
+	}
+
+	cmd := exec.Command(os.Args[0], "-test.run=TestErrorAndExit")
+	cmd.Env = append(os.Environ(), "TEST_EXIT=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+
+	t.Fatalf("process ran with err %v, want exit status 1", err)
+}
